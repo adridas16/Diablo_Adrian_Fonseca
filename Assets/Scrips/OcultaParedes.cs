52ba0f3510fda2f4c4a497657e8bb19346621b7d
adridas16 <adridas666@gmail.com>
2024-12-02 18:20:39 +0100

    luces Espejo

---
+++ Assets/Scrips/OcultaParedes.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcultaParedes : MonoBehaviour
+{
+    [SerializeField] private Renderer[] paredes;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (Renderer pared in paredes)
+            {
+               pared.enabled = false;
+            }
+        }
+    }
+}
