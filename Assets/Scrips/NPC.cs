1f36fbf52e256ac288ba0ea8884bf90e93ea7d85
adridas16 <adridas666@gmail.com>
2024-12-04 21:20:54 +0100

    no funciona NPC

--- Assets/Scrips/NPC.cs
+++ Assets/Scrips/NPC.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,40 @@ using UnityEngine;
 public class NPC : MonoBehaviour
 {
     // Start is called before the first frame update
+  
+    private Outline outline;
+    [SerializeField]private Texture2D cursorNPC;
+    [SerializeField]private Texture2D cursorIdl;
+    [SerializeField]float tiempoRotacion;
+    //Antes del start() (una vez)
+    //2 Este o no este el script habilitado,el Awake se lanza ,el Start NO. Para tus componentes 
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void Interactuar(Transform interactuador)
+    {
+        Debug.Log("hola");
+        transform.DOLookAt(interactuador.position, tiempoRotacion,AxisConstraint.Y);
+    }
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorNPC, Vector2.zero, CursorMode.Auto);
+        outline.enabled = true;       
+    }
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(cursorIdl, Vector2.zero, CursorMode.Auto);
+        outline.enabled = false;        
     }
 }
--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+    [SerializeField] private float distanciaInteraccion;
     private NavMeshAgent agent;
     private Camera cam;
+    //Guardo la informacion del NPC actual con el que voy a hablar
+    private NPC npcActual;
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        
     }
     void Start()
     {
@@ -21,15 +27,40 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Movimiento();
+        if (npcActual)
+        {
+            if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+            {
+                npcActual.Interactuar(this.transform);
+                npcActual = null;
+                agent.isStopped = true;
+                agent.stoppingDistance = 0;
+                
+            }
+        }
        
+    }
+
+    private void Movimiento()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             if (Input.GetMouseButtonDown(0))
             {
+                if (hit.transform.TryGetComponent(out NPC npc))
+                {
+                    //En ese caso ese npc es el actual.
+                    npcActual = npc;
+                    //Ahora mi distancia de parada es la de interaccion     
+                    agent.stoppingDistance = distanciaInteraccion;
+                }
+                //Mirrar a ver si el punto donde e impactado tiene el script NPC
                 agent.SetDestination(hit.point);
             }
-           
+
         }
     }
 }
